bdc830e887205d8779490e2368c6fa643629e951
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-05-30 22:24:17 -0300

    feat: API definition

--- src/schemap/__init__.py
+++ src/schemap/__init__.py
@@ -1,4 +1,18 @@
 """ Automatic Pydantic schemas for SQLAlchemy models """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
+
+from schemap.base import AutoBase, SchemaMixin
+from schemap.builder import build_schema
+from schemap.types import extract_python_type, extract_column_metadata
+from schemap.utils.likeness import ColumnLike
+
+__all__ = [
+    "AutoBase",
+    "SchemaMixin",
+    "build_schema",
+    "extract_python_type",
+    "extract_column_metadata",
+    "ColumnLike",
+]
 
